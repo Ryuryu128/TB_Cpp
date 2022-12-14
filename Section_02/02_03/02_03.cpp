45cc18afeb6cef418d76caddd8ef883ae64efcf7
Seohyun Ryu <atz999@ewhain.net>
2022-12-14 17:00:19 +0900

    02_04 고정너비 정수

---
+++ Section_02/02_03/02_03.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+// #include <cstdint>
+
+int main()
+{
+    // 고정 너비 정수
+    // 고정 너비 정수에 대한 내용은 필요할 때 좀 더 찾아보는 것으로..
+
+    using namespace std;
+
+    std::int16_t i(5);
+    std::int8_t myint = 65;
+
+    // cout << myint << endl;
+
+    std::int_fast8_t fi(5);
+    std::int_least64_t fl(5);
+
+}
