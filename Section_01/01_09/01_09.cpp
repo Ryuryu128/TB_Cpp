88efa7613c1cb8e5c61bef40bc563d199aa72753
Seohyun Ryu <atz999@ewhain.net>
2022-12-14 13:57:18 +0900

    01_09 기본적인 서식 맞추기

---
+++ Section_01/01_09/01_09.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// 변수들을 선언하고 하드코딩할 때 
+	// tab 키로 줄을 맞춰주면 가독성이 향상된다.
+	int x			= 1;
+	int my_v		= 4;
+	int num_apples	= 123;
+
+	// this is important
+	int x = 1 + 2; 
+
+	// 주석을 달 때는 가독성을 고려하며 달아주는 것 잊지 말기
+	int y = 2;
+
+    cout << "Hello World!\n";
+
+	return 0; 
+}
