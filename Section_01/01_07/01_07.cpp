d638fee4ef176abebfd422301657cec149a43d31
Seohyun Ryu <atz999@ewhain.net>
2022-12-14 13:34:43 +0900

    01_07 지역범위

---
+++ Section_01/01_07/01_07.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    cout << x << " " << &x << endl;
+
+    // 영역을 구분해 주는 중괄호
+    {
+        int x = 1;
+        cout << x << " " << &x << endl;
+    }
+
+    {
+        // int x = 2;
+        x = 2;
+        cout << x << " " << &x << endl;
+    }
+}
+
