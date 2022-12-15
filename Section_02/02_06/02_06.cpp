b577c73da2eca861ee10603d7c1d2ed94f776aa5
Seohyun Ryu <atz999@ewhain.net>
2022-12-15 14:07:31 +0900

    02_06 부리언 자료형과 조건문 if

---
+++ Section_02/02_06/02_06.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+bool isEqual(int a, int b)
+{
+	bool result = (a == b);
+	return result;
+}
+
+int main()
+{
+	using namespace std;
+
+	bool b1 = true;
+	bool b2(false);
+	bool b3{ true };
+
+	cout << b2 << endl;	// 0
+	cout << b3 << endl;	// 1
+
+	cout << std::boolalpha;	// bool	값을 0/1 이 아닌, true/false로 출력해 줌
+	cout << b2 << endl;	// false
+	cout << b3 << endl;	// true
+
+	cout << std::noboolalpha;	// bool	값을 true/false이 아닌, 0/1로 출력해 줌
+
+	// ------------------------------------------------------------- //
+
+	// Not Operator (!)
+	// false, true 앞에 !을 붙여주면 참 거짓 값이 바뀐다.
+	// !true == false
+	// !false == true
+
+	// ------------------------------------------------------------- //
+
+	cout << isEqual(0, 3) << endl;
+	cout << isEqual(1, 1) << endl;
+
+	// ------------------------------------------------------------- //
+
+	if (5)	// 0이 아니라면 모두 true값으로 인식한다.
+	{
+		cout << "True" << endl;	// 이 코드가 실행됨.
+	}
+	else
+	{
+		cout << "False" << endl;
+	}
+	
+}	
---
+++ Section_02/02_06_practice/02_06_practice.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void OddEven(int a)
+{
+	if (a % 2 == 0)
+	{
+		cout << a << " is Even" << endl;
+	}
+	else if (a % 2 == 1)
+	{
+		cout << a << " is Odd" << endl;
+	}
+	else
+		return;
+}
+
+int main()
+{
+	OddEven(1);
+	OddEven(4);
+}
